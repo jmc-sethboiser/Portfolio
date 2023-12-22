3e05623623360b746e1dbd1f16060743fe587c6b
jmc-sethboiser <sebastian.boiser@jmc.edu.ph>
2023-12-22 17:01:10 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Digital Artist.", "Producer.", "Frontend Developer.",
+    "Content Creator."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true })
